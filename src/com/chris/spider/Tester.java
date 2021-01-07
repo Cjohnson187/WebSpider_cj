af781b6213190abf17418e3f9584b2fe2b09864f
Chris Johnson <chris.johnson@siliconmtn.com>
2021-01-06 17:30:50 -0700

    just testing sockets

--- src/com/chris/spider/Tester.java
+++ src/com/chris/spider/Tester.java
@@ -1,8 +1,15 @@
 package com.chris.spider;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+
+
+import java.net.InetAddress;
+
 
 
 /****************************************************************************
@@ -24,18 +31,53 @@ public class Tester {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		try {
+			InetAddress site = InetAddress.getByName("www.christopherfjohnson.com");
+			System.out.println(site.getHostAddress());
+			Socket socket = new Socket(site.getHostAddress(), 443);
+			
+			sendCookie(socket);
+		} catch (Exception e) {
+			System.out.println("Exception - " + e);
+		}
+		
 
 	}
 	
-	public static void sendCoookie(Socket socket) throws IOException {
+	public static void sendCookie(Socket socket) throws IOException {
+		
+		// write to page
 		PrintWriter writer = new PrintWriter(socket.getOutputStream());
-		writer.println("HTTP/1.0 200 Ok");
-		writer.println("Set-Cookie key=value Max-Age=8640");
-		writer.println("Content-Type text/html");
-		writer.println(); //important
+		
+		// input stream to read response
+		BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		//
+		//writer.write ("GET /index.html HTTP/1.0\n\n");
+		
+		
+
+		writer.println("GET / HTTP/1.1");
+        writer.println("User-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT)");
+        writer.println("Host: www.christopherfjohnson.com");
+        writer.println("Accept-Language: en-us");
+        writer.println("Accept-Encoding: gzip, deflate");
+        writer.println("Connection: Keep-Alive");
+        writer.println();
+        writer.flush();
+		//writer.println("Host: christopherfjohnson.com");
+		//writer.println("Content-Type text/html");
+		//writer.println(); //important
 		//send your body here
-		writer.flush();//send message
+		//writer.flush();//send message
+		String line = "";
+		while((line = reader.readLine()) != null) {
+			System.out.println(line);
+		}
+		System.out.println("outy 5000");
+		
+		
 	}
 
 }
